b52b5f489a02b4ce5f19ad944466254ad1e749dc
Bob <r.baker1985@outlook.com>
2022-03-21 11:08:58 -0400

    motor code update

--- drive_setup.py
+++ drive_setup.py
@@ -1,17 +1,18 @@
 import serial
 from ticlib import TicSerial
+# from src.ticlib import TicSerial
 import time
 
 
 def setup_serial():
     port = serial.Serial("/dev/ttyS0", baud_rate=9600, timeout=0.1, write_timeout=0.1)
-    port1 = serial.Serial()
+    #  port1 = serial.Serial()
     tic = TicSerial(port)
     tic.reset()
     tic.clear_driver_error()
-    tic.setDecayMode(0)  # TicDecayMode auto_mixed
-    tic.setCurrentLimit(1000)  # en mA
+    tic.set_decay_mode('auto_mixed')  # TicDecayMode auto_mixed
+    tic.set_current_limit(1000)  # en mA
     # step mode: 0 = full, 1 = 1/2, 2 = 1/4, 3 = 1/8, 4 = 1/16, 5 = 1/32, 7 = 1/64, 8 = 1/128, 9 = 1/256
     # WARNING setting 6 is not in use on the 36V4
-    tic.setStepMode(5) # 5= 1/32
+    tic.set_step_mode(5) # 5= 1/32
     tic.energize()
